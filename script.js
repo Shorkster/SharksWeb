d0a65551be78bb7130d73cc6f0e57960ffb3cf43
Sharky <sharkinc0806@gmail.com>
2025-03-14 22:47:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    this.style.backgroundColor = "black";
+    document.getElementById("message").textContent = "Boom!";
+}); 
