b4a80ae22053d19ecda8791a4895ce101837bd1e
Sharky <sharkinc0806@gmail.com>
2025-03-15 15:52:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-document.getElementById("myButton").addEventListener("click", function() {
-    this.style.backgroundColor = "black";
-    document.getElementById("message").textContent = "Boom!";
-}); 
+// Empty for now
